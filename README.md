5bd7341a3e405d19bb56d44486db00f0a7843079
Mykel Kochenderfer <mykel@stanford.edu>
2014-06-16 23:11:37 -0700

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PGFPlots
+
+This library uses the LaTeX package [pgfplots](http://ctan.org/pkg/pgfplots) to produce plots. It integrates with IJulia, outputting SVG images to the notebook.
+
+In order to use this package, you need to install TikzPictures by running `Pkg.clone("https://github.com/sisl/TikzPictures.jl)`. You also need to have the pgfplots package installed.
+
+## Example
+
+```julia
+using PGFPlots
+a = [1, 2, 3]
+b = [1, 3, 2]
+plot(a, b)
+```
--- src/PGFPlots.jl
+++ src/PGFPlots.jl
@@ -1,5 +1,61 @@
 module PGFPlots
 
-# package code goes here
+export plot, Axis
+
+using TikzPictures
+
+type Plot
+  data
+end
+
+type Axis
+  plots
+  title::String
+  xlabel::String
+  ylabel::String
+  Axis(data; title="", xlabel="", ylabel="") = new([Plot(data)],
+  title, xlabel, ylabel
+  )
+end
+
+function optionString(axis::Axis)
+  t = ""
+  first = true
+  for n in names(Axis)
+    if n != :plots && length(axis.(n)) > 0
+      if first
+        first = false
+      else
+        t *= ", "
+      end
+      t *= "$(string(n)) = $(axis.(n))"
+    end
+  end
+  if length(t) > 0
+    t = "[$t]"
+  else
+    return ""
+  end
+end
+
+function plot(axis::Axis)
+  t = "\\begin{axis}$(optionString(axis))\n"
+  for p in axis.plots
+    t *= "\\addplot coordinates {\n"
+    for i = 1:size(p.data,2)
+      t *= "($(p.data[1,i]), $(p.data[2,i]))\n"
+    end
+    t *= "};\n"
+  end
+  t *= "\\end{axis}"
+  preamble = "\\usepackage{pgfplots}\n"
+  preamble *= "\\pgfplotsset{compat=newest}"
+  TikzPicture(t, options="scale=1", preamble=preamble)
+end
+
+plot(data) = plot(Axis(data))
+
+plot(x::AbstractArray, y::AbstractArray) = plot([x y]')
+
 
 end # module
