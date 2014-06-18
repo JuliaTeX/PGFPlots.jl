967c1d04015abb57521270d71059e0438e13643e
Mykel Kochenderfer <mykel@stanford.edu>
2014-06-17 17:20:14 -0700

    Added histograms

--- README.md
+++ README.md
@@ -2,13 +2,38 @@
 
 This library uses the LaTeX package [pgfplots](http://ctan.org/pkg/pgfplots) to produce plots. It integrates with IJulia, outputting SVG images to the notebook.
 
-In order to use this package, you need to install TikzPictures by running `Pkg.clone("https://github.com/sisl/TikzPictures.jl)`. You also need to have the pgfplots package installed.
+In order to use this package, you need to install TikzPictures by running `Pkg.clone("https://github.com/sisl/TikzPictures.jl)`. You also need to have the pgfplots package (version 1.10 or later) installed.
 
-## Example
+## Examples
 
+### Basic plot
 ```julia
 using PGFPlots
 a = [1, 2, 3]
 b = [1, 3, 2]
 plot(a, b)
+```
+
+### Group plots
+```julia
+using PGFPlots
+using Distributions
+# define a normal distribution with mean 3 and standard deviation 2
+dist = Normal(3,2)
+# generate 1000 samples
+d = rand(dist, 1000)
+# try out histograms with a variety of different number of bins
+bins = [3 10 50 1000]
+g = GroupPlot(2,2)
+for i = 1:length(bins)
+    push!(g, Axis(Plots.Histogram(d, bins=bins[i]), ymin=0))
+end
+g
+```
+
+### Function plots
+
+```julia
+using PGFPlots
+plot(sin,(0,10))
 ```
--- src/PGFPlots.jl
+++ src/PGFPlots.jl
@@ -1,28 +1,94 @@
 module PGFPlots
 
-export plot, Axis
+export plot, Axis, GroupPlot, Plots
+
+# include("ndgrid.jl")
+
+# import Images: grayim, imwrite
+
+preamble = """
+\\usepackage{pgfplots}
+\\pgfplotsset{compat=newest}
+"""
 
 using TikzPictures
 
-type Plot
-  data
+module Plots
+
+export Plot, Histogram, Linear
+
+abstract Plot
+
+type Histogram <: Plot
+  data::AbstractArray{Real,1}
+  bins::Integer
+  density::Bool
+  cumulative::Bool
+  style::String
+  Histogram(data; bins=20, density=false, cumulative=false, style="fill=blue!10") = new(data, bins, density, cumulative, style)
 end
 
+type Linear <: Plot
+  data::AbstractArray{Real,2}
+  mark::String
+  style::String
+  Linear(data::AbstractArray{Real,2}; mark="", style="") = new(data, mark, style)
+  Linear{T<:Real}(x::AbstractArray{T,1}, y::AbstractArray{T,1}; mark="", style="") = new([x y]', mark, style)
+end
+
+# type Image <: Plot
+#   filename::String
+#   xmin::Real
+#   xmax::Real
+#   ymin::Real
+#   ymax::Real
+# end
+
+end # end plot module
+
+using .Plots
+
 type Axis
-  plots
+  plots::AbstractArray{Plot,1}
   title::String
   xlabel::String
   ylabel::String
-  Axis(data; title="", xlabel="", ylabel="") = new([Plot(data)],
-  title, xlabel, ylabel
+  xmin::Real
+  ymin::Real
+  Axis(plot::Plot;title="", xlabel="", ylabel="", xmin=NaN, ymin=NaN) = new([plot],
+  title, xlabel, ylabel, xmin, ymin
   )
 end
 
-function optionString(axis::Axis)
+type GroupPlot
+  axes::AbstractArray{Axis,1}
+  dimensions::(Integer,Integer)
+  GroupPlot() = new(AbstractArray{Axis,1}[], (0,0))
+  GroupPlot(axes::AbstractArray{Axis,1}) = new(axes, (length(axes), 1))
+  GroupPlot(plots) = new([Axis(p) for p in plots], (length(plots), 1))
+  GroupPlot(dimensions::(Integer,Integer)) = new(AbstractArray{Axis,1}[], dimensions)
+  GroupPlot(rows::Integer, columns::Integer) = new(AbstractArray{Axis,1}[], (rows, columns))
+end
+
+function Base.push!(g::GroupPlot, a::Axis)
+  push!(g.axes, a)
+  if length(g.axes) > prod(g.dimensions)
+    g.dimensions = (length(g.plots), 1)
+  end
+end
+
+function Base.push!(g::GroupPlot, p::Plot)
+  push!(g, Axis(p))
+end
+
+empty(a::Real) = isnan(a)
+empty(a::String) = length(a) == 0
+
+function optionString(o::IOBuffer, axis::Axis)
   t = ""
   first = true
   for n in names(Axis)
-    if n != :plots && length(axis.(n)) > 0
+    if n != :plots && !empty(axis.(n))
       if first
         first = false
       else
@@ -32,30 +98,95 @@ function optionString(axis::Axis)
     end
   end
   if length(t) > 0
-    t = "[$t]"
-  else
-    return ""
+    print(o, "[$t]")
+  end
+end
+
+function plotHelper(o::IOBuffer, p::Histogram)
+  println(o, "\\addplot+ [mark=none, $(p.style), hist={bins=$(p.bins),density=$(p.density),cumulative=$(p.cumulative)}] table [row sep=\\\\, y index = 0] {")
+  println(o, "data\\\\")
+  for d in p.data
+    print(o, "$d \\\\ ")
+  end
+  println(o, "};")
+end
+
+
+
+function plotHelper(o::IOBuffer, p::Linear)
+  print(o, "\\addplot+ ")
+  if !empty(p.mark) || !empty(p.style)
+    print(o, "[$(p.style), mark=$(p.mark)] ")
+  end
+  println(o, "coordinates {")
+  for i = 1:size(p.data,2)
+    println(o, "($(p.data[1,i]), $(p.data[2,i]))")
+  end
+  println(o, "};")
+end
+
+function plotHelper(o::IOBuffer, p::Image)
+  println(o, "\\addplot graphics [xmin=$(p.xmin), xmax=$(p.xmax), ymin=$(p.ymin), ymax=$(p.ymax)] {$(p.filename)};")
+end
+
+# plot option string and contents; no \begin{axis} or \nextgroupplot
+function plotHelper(o::IOBuffer, axis::Axis)
+  optionString(o, axis)
+  for p in axis.plots
+      plotHelper(o, p)
   end
 end
 
 function plot(axis::Axis)
-  t = "\\begin{axis}$(optionString(axis))\n"
-  for p in axis.plots
-    t *= "\\addplot coordinates {\n"
-    for i = 1:size(p.data,2)
-      t *= "($(p.data[1,i]), $(p.data[2,i]))\n"
-    end
-    t *= "};\n"
-  end
-  t *= "\\end{axis}"
-  preamble = "\\usepackage{pgfplots}\n"
-  preamble *= "\\pgfplotsset{compat=newest}"
-  TikzPicture(t, options="scale=1", preamble=preamble)
+  o = IOBuffer()
+  print(o, "\\begin{axis}")
+  plotHelper(o, axis)
+  println(o, "\\end{axis}")
+  TikzPicture(takebuf_string(o), options="scale=1", preamble=preamble)
 end
 
-plot(data) = plot(Axis(data))
+function plot(p::GroupPlot)
+  o = IOBuffer()
+  println(o, "\\begin{groupplot}[group style={group size=$(p.dimensions[1]) by $(p.dimensions[2])}]")
+  for a in p.axes
+    print(o, "\\nextgroupplot ")
+    plotHelper(o, a)
+  end
+  println(o, "\\end{groupplot}")
+  mypreamble = preamble * "\\usepgfplotslibrary{groupplots}"
+  TikzPicture(takebuf_string(o), options="scale=1", preamble=mypreamble)
+end
 
-plot(x::AbstractArray, y::AbstractArray) = plot([x y]')
+# function Plots.Image(f::Function, xrange::(Real,Real), yrange::(Real,Real); filename="tmp.png")
+#   x = linspace(xrange[1], xrange[2])
+#   y = linspace(yrange[1], yrange[2])
+#   (X, Y) = meshgrid(x, y)
+#   A = map(f, Y, X)
+#   # normalize A
+#   A = A .- minimum(A)
+#   A = A ./ maximum(A)
+#   imwrite(grayim(fliplr(A)), filename)
+#   Image(filename, xrange[1], xrange[2], yrange[1], yrange[2])
+# end
+
+plot(p::Plot) = plot(Axis(p))
+
+plot{T<:Real}(x::AbstractArray{T,1}, y::AbstractArray{T,1}) = plot(Linear(x, y))
+
+function plot(f::Function, range::(Real,Real))
+  x = linspace(range[1], range[2])
+  y = map(f, x)
+  plot(Linear(x, y, mark="none"))
+end
+
+Base.mimewritable(::MIME"image/svg+xml", p::Plot) = true
+Base.writemime(f::IO, a::MIME"image/svg+xml", p::Plot) = Base.writemime(f, a, plot(p))
+
+Base.mimewritable(::MIME"image/svg+xml", p::GroupPlot) = true
+Base.writemime(f::IO, a::MIME"image/svg+xml", p::GroupPlot) = Base.writemime(f, a, plot(p))
+
+Base.mimewritable(::MIME"image/svg+xml", p::Axis) = true
+Base.writemime(f::IO, a::MIME"image/svg+xml", p::Axis) = Base.writemime(f, a, plot(p))
 
 
 end # module
