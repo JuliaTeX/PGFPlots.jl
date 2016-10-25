12631934b709c70d8afa171034315ce7c1a49088
Tim Wheeler <timwheeleronline@gmail.com>
2016-10-25 17:06:17 +0200

    adding Reel gif

--- src/PGFPlots.jl
+++ src/PGFPlots.jl
@@ -9,6 +9,7 @@ export pushPGFPlotsPreamble, popPGFPlotsPreamble, resetPGFPlotsPreamble, pgfplot
 export pushPGFPlots, popPGFPlots
 import Colors: RGB
 import Contour: contours, levels
+import Reel: extension
 
 using Compat
 using Discretizers
@@ -735,5 +736,10 @@ function save(filename::AbstractString, o::Plottable; include_preamble::Bool=tru
         error("Unsupported file extensions: $ext")
     end
 end
+
+###################################################
+# Reel configuration
+
+extension(m::MIME"image/svg+xml") = "svg"
 
 end # module
