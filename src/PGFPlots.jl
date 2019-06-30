c6f26d4a047dd01a2d5b224184990d03c731b83f
Tim Wheeler <timwheeleronline@gmail.com>
2019-06-29 17:06:19 -0700

    ternary axis

--- src/PGFPlots.jl
+++ src/PGFPlots.jl
@@ -1,7 +1,7 @@
 module PGFPlots
 
 export LaTeXString, @L_str, @L_mstr
-export plot, ErrorBars, Axis, Axes, PolarAxis, GroupPlot, Plots, ColorMaps, save, define_color
+export plot, ErrorBars, Axis, Axes, PolarAxis, TernaryAxis, GroupPlot, Plots, ColorMaps, save, define_color
 export pushPGFPlotsOptions, popPGFPlotsOptions, resetPGFPlotsOptions, pgfplotsoptions
 export pushPGFPlotsPreamble, popPGFPlotsPreamble, resetPGFPlotsPreamble, pgfplotspreamble
 export pushPGFPlots, popPGFPlots
@@ -218,6 +218,13 @@ mutable struct Axis
 end
 
 PolarAxis(args...; kwargs...) = Axis(args...; kwargs..., axisKeyword = "polaraxis")
+function TernaryAxis(args...; kwargs...)
+    ternary_axis_string = "\\usepgfplotslibrary{ternary}"
+    if ternary_axis_string ∉ _pgfplotspreamble
+        pushPGFPlotsPreamble(ternary_axis_string)
+    end
+    return Axis(args...; kwargs..., axisKeyword = "ternaryaxis")
+end
 
 const Axes = Vector{Axis}
 
