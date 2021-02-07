e84aacf885904e7b0a7a83617e8d91591827b9fb
Tim Wheeler <timwheeleronline@gmail.com>
2021-02-07 14:51:14 -0800

    curious whether this works

--- src/PGFPlots.jl
+++ src/PGFPlots.jl
@@ -12,6 +12,11 @@ using Requires
 using Discretizers
 using Printf
 
+# Define isnothing
+if VERSION < v"1.1"
+    isnothing(x) = x === nothing
+end
+
 # load dynamic dependencies
 function __init__()
     @require DataFrames="a93c6f00-e57d-5684-b7b6-d8193f3e46c0" include("dataframes.jl")
